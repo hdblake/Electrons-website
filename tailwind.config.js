314cd525d46449e2b1bc7a7aa304235d5531365a
= <=>
2023-06-30 16:15:51 -0500

    updated tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.{html,js,jsx} ./src/**/*.{html,js,jsx}"],
+  content: ["./src/index.html", 
+  "./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      "main": "#1A71BD",
+      "secondary": "#FFDC00",
+      "accent": "#ffffff"
+    },
     extend: {},
   },
   plugins: [],
