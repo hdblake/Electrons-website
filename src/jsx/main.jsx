d2ca3303cdbb77c216aeb7df33b377e6f4815f0a
= <=>
2023-07-11 12:41:38 -0500

    added templates

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Header from "../templates/header.jsx";
+import Footer from "../templates/footer.jsx";
 import "../styles/index.css";
+// import { loadHeaderFooter } from "./utils.mjs";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
