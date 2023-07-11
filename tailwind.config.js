eca429c21ee83e3f1b8d897d6a66e0a04f03bd6a
Hunter <hdblake333@gmail.com>
2023-07-10 22:43:04 -0500

    ran format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      "main": "#1A71BD",
-      "secondary": "#FFDC00",
-      "accent": "#ffffff"
+      main: "#1A71BD",
+      secondary: "#FFDC00",
+      accent: "#ffffff",
     },
     extend: {},
   },
   plugins: [],
-}
+};
