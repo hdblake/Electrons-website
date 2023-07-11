31e52a31ff6d703d84b9d4fb2438603b1c0bee71
= <=>
2023-07-11 12:49:32 -0500

    removed img

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,17 +1,6 @@
-import mainLogo from "../images/electrons-main-logo.webp";
-
 function App() {
   return (
     <>
-      <div>
-        <img
-          src={mainLogo}
-          className="logo"
-          alt="main logo"
-          width="275"
-          height="39"
-        />
-      </div>
       <h1>Welcome to Electrons!</h1>
       <div className="card"></div>
     </>
