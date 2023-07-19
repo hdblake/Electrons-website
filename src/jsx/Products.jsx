26f844299f064f1d1f1a54394130a1be3c44273e
Hunter <hdblake333@gmail.com>
2023-07-18 23:08:49 -0500

    updated styles and function

--- src/jsx/Products.jsx
+++ src/jsx/Products.jsx
@@ -16,17 +16,37 @@ export default function GetProducts() {
 
   return (
     <>
-      <h1>Top Products: {categoryCapitalized}</h1>
-      {products &&
-        products.map((product) => (
-          <div key={product.id}>
-            <img src={product.thumbnail} alt={"Image of" + product.title} />
-            <p>
-              {product.brand} {product.title}
-            </p>
-            <p>Price: ${product.price}</p>
-          </div>
-        ))}
+      <h1 className="md:text-4xl text-3xl text-center mx-3 mb-8 text-main font-bold underline decoration-secondary">
+        Top Products: {categoryCapitalized}
+      </h1>
+      <div className="product-wrapper grid gap-8 auto-rows-fr md:grid-cols-2 sm:grid-cols-1 place-items-center justify-items-center items-center">
+        {products &&
+          products.map((product) => (
+            <div
+              key={product.id}
+              className="text-main flex flex-col justify-center items-center rounded-2xl border-4 border-main md:w-3/4 mx-4 pb-4 h-100 h-full mt-auto"
+            >
+              <img
+                src={product.thumbnail}
+                alt={"Image of" + product.title}
+                className="rounded-t-xl mb-auto"
+              />
+              <p className="font-bold text-xl m-2 mb-auto underline decoration-secondary decoration-2">
+                {product.title}
+              </p>
+              <p className="font-bold mt-2 mb-auto text-lg m-2">
+                Price: ${product.price}
+              </p>
+              <button
+                id="addToCart"
+                data-id=""
+                className="bg-main py-3 px-5 text-secondary font-bold rounded-lg"
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
