e08a144728e78f81326547e51d7c9b319bde804b
Hunter <hdblake333@gmail.com>
2023-07-18 22:25:51 -0500

    added/update style

--- src/templates/header.jsx
+++ src/templates/header.jsx
@@ -1,21 +1,29 @@
 import mainLogo from "../images/electrons-main-logo.webp";
 import { Link } from "react-router-dom";
+import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
 
 export default function Header() {
   return (
-    <>
+    <div className="logo-cart-wrapper flex justify-between mx-3 mb-9 mt-3">
       <div className="logo">
         <Link to="/">
           <img
             src={mainLogo}
             className="logo"
             alt="main logo"
-            width="275"
-            height="39"
+            width="240"
+            height="34"
           />
         </Link>
       </div>
-      <div className="cart"></div>
-    </>
+      <div className="cart">
+        <ShoppingCartSharpIcon
+          className="text-main"
+          style={{
+            fontSize: "32px",
+          }}
+        />
+      </div>
+    </div>
   );
 }
