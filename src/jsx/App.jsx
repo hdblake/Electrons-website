2087b1cd305d09d8b0de0e690a25384e19eec50f
Hunter <hdblake333@gmail.com>
2023-07-18 19:40:19 -0500

    added routes

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
+import Products from "./Products.jsx";
+
 function App() {
   return (
-    <>
-      <h1>Welcome to Electrons!</h1>
-      <div className="card"></div>
-    </>
+    <Routes>
+      <Route index element={<Home />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/products/:category" element={<Products />}></Route>
+    </Routes>
   );
 }
 
