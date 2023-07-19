e0eea5e1d765043f6ecad7803f0d44711bd5d466
Hunter <hdblake333@gmail.com>
2023-07-18 23:05:31 -0500

    updated margin

--- src/templates/header.jsx
+++ src/templates/header.jsx
@@ -4,7 +4,7 @@ import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
 
 export default function Header() {
   return (
-    <div className="logo-cart-wrapper flex justify-between mx-3 mb-9 mt-3">
+    <div className="logo-cart-wrapper flex justify-between mx-2 mb-9 mt-3">
       <div className="logo">
         <Link to="/">
           <img
