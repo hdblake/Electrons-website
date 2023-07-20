b1b0cb18ccd28bf906cc0b16077e6ea96b3b3f72
Hunter <hdblake333@gmail.com>
2023-07-19 20:58:06 -0500

    added cart & checkout route

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import Products from "./Products.jsx";
+import Cart from "./Cart.jsx";
+import Checkout from "./Checkout.jsx";
+// import { useState } from "react";
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <Route index element={<Home />}></Route>
       <Route path="/home" element={<Home />}></Route>
       <Route path="/products/:category" element={<Products />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/checkout" element={<Checkout />}></Route>
     </Routes>
   );
 }
