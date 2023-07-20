c8d196bacf542bdc3e40123ca27ef3fc53f194f6
= <=>
2023-07-20 15:21:05 -0500

    removed pages

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        productList: resolve(__dirname, "src/pages/ProductList.jsx"),
       },
     },
   },
